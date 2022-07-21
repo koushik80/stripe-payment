c09f31484282d20d1ca098cb4243fd7ed3e4718e
koushik <koushik.dey@live.com>
2022-07-22 01:08:48 +0300

    pay-2

--- src/Pay.jsx
+++ src/Pay.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 const Pay = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div
+            style={{
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <button
+                style={{
+                    border: "none",
+                    width: 120,
+                    borderRadius: 5,
+                    padding: "20px",
+                    backgroundColor: "black",
+                    color: "white",
+                    fontWeight: "600",
+                    cursor: "pointer",
+                }}
+            >
+                Pay Now
+            </button>
+        </div>
+    );
+};
 
 export default Pay
 
